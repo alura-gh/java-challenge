4b14cc20f66c6d2c2aca353bfaedfe77bf3234b3
tineojf <92743762+tineodev@users.noreply.github.com>
2025-04-19 19:14:29 -0500

    feat(converter): delete unused methos

--- src/Converter.java
+++ src/Converter.java
@@ -12,29 +12,9 @@ import java.util.Scanner;
 public class Converter {
     private static String API_KEY;
     private final String API_URL;
-    private String base_code;
-    private String target_code;
 
     public Converter(String base_code, String target_code) {
-        this.base_code = base_code;
-        this.target_code = target_code;
         this.API_URL = "https://v6.exchangerate-api.com/v6/" + Converter.API_KEY + "/pair/" + base_code + "/" + target_code;
-    }
-
-    public String getBase_code() {
-        return base_code;
-    }
-
-    public void setBase_code(String base_code) {
-        this.base_code = base_code;
-    }
-
-    public String getTarget_code() {
-        return target_code;
-    }
-
-    public void setTarget_code(String target_code) {
-        this.target_code = target_code;
     }
 
     // methods
